53bedd67c39f4bc1ec21509ee0720d30eb2e2e4e
schunck <a1239326826@163.com>
2021-04-12 09:11:38 +0800

    新数据

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
-    <router-view />
+    <div>
+      <i class="toutiao toutiao-shanchu"></i>
+    </div>
+
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
